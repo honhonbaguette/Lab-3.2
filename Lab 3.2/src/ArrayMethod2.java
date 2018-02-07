640a2af4cf780bbeef20365fee25367b7aa709a2
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-02-07 16:57:42 -0500

    error

---
+++ Lab 3.2/src/ArrayMethod2.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class ArrayMethod2 
+{
+	public static void main(String[] args)
+	{
+		String[] list1 = {"A","B","C","D","E","F"};
+		String[] list2 = {"G","H","I","J","K","L"};
+		for (int i = 0; i <list1.length + list2.length; i++)
+		{
+			System.out.print(merge(list1.length + list2.length));
+		}
+	}
+	public static String[] merge(String[]list1, String[]list2)
+	{		
+		String[] list3 = new String(list1.length + list2.length)
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		while (x < list1.length && y < list2.length) 
+	      {
+	           if (list1[z].compareTo(list2[y]) < 0) 
+	           {
+	               list3[z] = list1[y];
+	               x++;
+	           } 
+	           else 
+	           {
+	               list3[z] = list2[y];
+	               y++;
+	           }
+	           z++;
+	      }
+	      return list3;
+	
+	}
+	
+	public static int partition(int[] list)
+	{
+		return (Integer) 0;
+
+	}
+
+}
